cd4bc1e092637ff92058c7f145401bc501280065
Heber Uriel <hurielarrascaeta@gmail.com>
2025-06-20 19:15:38 -0300

    Fifth Commit

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import BlogHomePageView, PostDetailView
+from .views import BlogHomePageView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CategoryCreateView
 
-app_name = 'blog'
+app_name = 'django_blog'
 urlpatterns = [
     path('', BlogHomePageView.as_view(), name='home'),
-    path('<slug:slug>/', PostDetailView.as_view(), name='post_detail')
+    path('post/new/', PostCreateView.as_view(),
+         name='post_create'),
+    path('category/new/', CategoryCreateView.as_view(), name='category_create'),
+    path('post/<slug:slug>/update/', PostUpdateView.as_view(),
+         name='post_update'),
+    path('post/<slug:slug>/delete/', PostDeleteView.as_view(),
+         name='post_delete'),
+    path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
